b335382ad1a02916890804ab246e39b2fd83ea6c
Anthony Williams <anthony@madebypurple.com>
2011-10-23 13:32:41 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeautifulwpApp::Application.config.secret_token = '4628a42b08c871df6f8015dd16ca1fb8f9df482b848d22e5e43fe434ffaf3f08096fe021efba19801dfb03fe0a13a43a5fa5826ea16646be4b2e73f8e375fe2f'
