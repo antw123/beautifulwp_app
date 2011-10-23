82dca712cea706633e9d45e52c39b926ff9d7451
Anthony Williams <anthony@madebypurple.com>
2011-10-23 16:09:26 -0400

    Added layout home about contact pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,18 @@
 class PagesController < ApplicationController
   
   def home
+    @title = "Home"
   end
 
   def about
+    @title = "About"
   end
 
   def submit
   end
 
   def contact
+    @title = "Contact"
   end
 
 end
