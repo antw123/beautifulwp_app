b18ad2c45555aa89cd16f3072a0b81c3b77b7cb2
Anthony Williams <anthony@madebypurple.com>
2011-11-03 19:29:17 -0400

    Working index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
   
   def index
+    @users = User.all
+    @title = "All Users"
   end
   
   def new
