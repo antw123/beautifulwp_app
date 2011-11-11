d3b4af897b2aba6197117d0c16d29f7be64c06e8
Anthony Williams <anthony@madebypurple.com>
2011-11-11 15:22:50 -0500

    Added the ability to add a remote image url to the new and edit actions

--- app/models/showcase.rb
+++ app/models/showcase.rb
@@ -3,7 +3,7 @@ class Showcase < ActiveRecord::Base
   attr_accessible :title, :author, :feature1, :feature2, :feature3, :feature4, 
                                    :related1, :related_url1, :related2, :related_url2, :related3, 
                                    :related_url3, :related4, :related_url4, :price, :description, 
-                                   :site, :site_url, :image_url, :image
+                                   :site, :site_url, :image_url, :image, :remote_image_url
   
   belongs_to :user
   mount_uploader :image, ImageUploader
