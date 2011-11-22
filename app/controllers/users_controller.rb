696b6a9d6b3cbb4515610bae78e82245867bb5f6
Anthony Williams <anthony@madebypurple.com>
2011-11-22 01:04:44 -0500

    Locked app so users can't create accounts unless admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:index, :show, :edit, :update, :destroy]
+  before_filter :authenticate
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user,   :only => [:index, :destroy]
   
