92bab897b95591bc6eb264d46822e4198b7ed52b
Anthony Williams <anthony@madebypurple.com>
2012-02-19 14:37:11 -0500

    Changed title tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   #Return a title on a per-page basis.
   def title
-    base_title = "WordPress Theme Gallery | Beautiful Free & Premium Wordpress Themes"
+    base_title = "WordPress Theme Gallery | Free & Premium Wordpress Themes | Best WordPress Themes"
     if @title.nil?
       base_title
     else
